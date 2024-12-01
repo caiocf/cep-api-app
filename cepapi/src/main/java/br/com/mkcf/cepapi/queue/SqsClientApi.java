878c8f21362cd13582f9e8c7059c56e0305f6457
Caio Ferreira <caio.c.f@hotmail.com>
2024-12-01 19:42:26 -0300

    feat: Create SQS Producer set properties

--- cepapi/src/main/java/br/com/mkcf/cepapi/queue/SqsClientApi.java
+++ cepapi/src/main/java/br/com/mkcf/cepapi/queue/SqsClientApi.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import br.com.mkcf.cepapi.model.CepResponse;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
+//import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -17,12 +17,12 @@ public class SqsClientApi {
 	@Value("${sqs.queue.host}")
 	private String queueHost;
 
-	private final SqsTemplate sqsTemplate;
+	//private final SqsTemplate sqsTemplate;
 
 	public void sendMessage(CepResponse cepResponse) {
 
-		sqsTemplate.send(queueHost, new MessageCreatedEvent(cepResponse,
-				new SimpleDateFormat("yyyy/MM/dd hh:mm:ss yyy").format(new Date())));
+	//	sqsTemplate.send(queueHost, new MessageCreatedEvent(cepResponse,
+		//		new SimpleDateFormat("yyyy/MM/dd hh:mm:ss yyy").format(new Date())));
 	}
 
 }
--- cepapi/src/main/java/br/com/mkcf/cepapi/queue/SqsContext.java
+++ cepapi/src/main/java/br/com/mkcf/cepapi/queue/SqsContext.java
@@ -1,19 +1,10 @@
 package br.com.mkcf.cepapi.queue;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class SqsContext {
 
-	@Bean
-	public SqsClient createSqsClient() {
-		return SqsClient.builder().region(Region.SA_EAST_1).credentialsProvider(ProfileCredentialsProvider.create())
-				.build();
-	}
+	
 
 }
