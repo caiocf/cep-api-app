454427c317752c98abcc64933f331891a2adcea2
Caio Ferreira <caio.c.f@hotmail.com>
2024-12-08 20:06:49 -0300

    feat: ajuste pasta para root level.

--- cepapi/src/main/java/br/com/mkcf/cepapi/exception/CepNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.mkcf.cepapi.exception;
-
-public class CepNotFoundException extends RuntimeException {
-    public CepNotFoundException(String message) {
-        super(message);
-    }
-}
--- cepapi/src/main/java/br/com/mkcf/cepapi/model/dto/SqsMessageProcessor.java
+++
@@ -1,5 +0,0 @@
-package br.com.mkcf.cepapi.model.dto;
-
-public record SqsMessageProcessor(CepResponse cep, String codigoCep){
-
-}
